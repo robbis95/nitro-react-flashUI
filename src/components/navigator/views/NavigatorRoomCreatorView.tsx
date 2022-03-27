76d22a87dcc5649afd1396b5519cb7d13cf3f4d1
robbis95 <82042925+robbis95@users.noreply.github.com>
2022-03-27 19:07:46 +0200

    bug fixes

--- src/components/navigator/views/NavigatorRoomCreatorView.tsx
+++ src/components/navigator/views/NavigatorRoomCreatorView.tsx
@@ -2,7 +2,7 @@
 import { HabboClubLevelEnum, RoomCreateComposer } from '@nitrots/nitro-renderer';
 import { FC, useEffect, useState } from 'react';
 import { GetClubMemberLevel, GetConfiguration, IRoomModel, LocalizeText, RoomModels, SendMessageComposer } from '../../../api';
-import { Button, Column, Flex, Grid, LayoutCurrencyIcon, LayoutGridItem, Text } from '../../../common';
+import { AutoGrid, Button, Column, Flex, Grid, LayoutCurrencyIcon, LayoutGridItem, NitroCardContentView, NitroCardHeaderView, NitroCardView, Text } from '../../../common';
 import { BatchUpdates } from '../../../hooks';
 import { useNavigatorContext } from '../NavigatorContext';
 
