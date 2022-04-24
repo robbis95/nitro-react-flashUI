0f2ec2aadaf525521ea833c5ace1de8edbdfd59b
robbis95 <82042925+robbis95@users.noreply.github.com>
2022-04-24 11:44:33 +0200

    Styled highscore

--- src/components/room/widgets/furniture/high-score/FurnitureHighScoreView.tsx
+++ src/components/room/widgets/furniture/high-score/FurnitureHighScoreView.tsx
@@ -66,37 +66,41 @@ export const FurnitureHighScoreView: FC<{}> = props =>
             {
                 return (
                     <ObjectLocationView key={ index } objectId={ objectId } category={ RoomObjectCategory.FLOOR }>
-                        <Column className="nitro-widget-high-score nitro-context-menu" gap={ 0 }>
-                            <ContextMenuHeaderView>
-                                { LocalizeText('high.score.display.caption', [ 'scoretype', 'cleartype' ], [ LocalizeText(`high.score.display.scoretype.${ SCORE_TYPES[stuffData.scoreType] }`), LocalizeText(`high.score.display.cleartype.${ CLEAR_TYPES[stuffData.clearType] }`) ]) }
-                            </ContextMenuHeaderView>
+                        <Column className="nitro-widget-high-score" gap={ 0 }>
+                            <Flex center className="nitro-widget-high-score-header">
+                                <Text small bold>{ LocalizeText('high.score.display.caption', [ 'scoretype', 'cleartype' ], [ LocalizeText(`high.score.display.scoretype.${ SCORE_TYPES[stuffData.scoreType] }`), LocalizeText(`high.score.display.cleartype.${ CLEAR_TYPES[stuffData.clearType] }`) ]) }</Text>
+                            </Flex>
                             <ContextMenuListView overflow="hidden" gap={ 1 } className="h-100">
                                 <Column gap={ 1 }>
-                                    <Flex alignItems="center">
-                                        <Text center bold variant="white" className="col-8">
+                                    <Flex alignItems="center" className="score-board-header mt-1 p-1">
+                                        <Text small variant="black" className="col-8">
                                             { LocalizeText('high.score.display.users.header') }
                                         </Text>
-                                        <Text center bold variant="white" className="col-4">
+                                        <Text small variant="black" className="col-4">
                                             { LocalizeText('high.score.display.score.header') }
                                         </Text>
                                     </Flex>
                                     <hr className="m-0" />
                                 </Column>
-                                <Column overflow="auto" gap={ 1 } className="overflow-y-scroll">
+                                <Column overflow="auto" gap={ 1 } className="overflow-y-auto high-score-results">
                                     { stuffData.entries.map((entry, index) =>
                                     {
                                         return (
                                             <Flex key={ index } alignItems="center">
-                                                <Text center variant="white" className="col-8">
+                                                <Text small variant="white" className="col-8">
                                                     { entry.users.join(', ') }
                                                 </Text>
-                                                <Text center variant="white" className="col-4">
+                                                <Text small variant="white" className="col-4">
                                                     { entry.score }
                                                 </Text>
                                             </Flex>
                                         );
                                     }) }
                                 </Column>
+                                <i className="trophy position-absolute"/>
+                                <Flex center className="bottom-text">
+                                    <Text small center>{LocalizeText('high.score.display.congratulations.footer')}</Text>
+                                </Flex>
                             </ContextMenuListView>
                         </Column>
                     </ObjectLocationView>
