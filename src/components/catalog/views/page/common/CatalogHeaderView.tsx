899c388e202ac9b9e978f3b43e818731aec1b53d
Layne <laynebalsters@gmail.com>
2022-04-26 10:09:35 -0400

    fix cata icon n name

--- src/components/catalog/views/page/common/CatalogHeaderView.tsx
+++ src/components/catalog/views/page/common/CatalogHeaderView.tsx
@@ -12,14 +12,14 @@ export interface CatalogHeaderViewProps
 export const CatalogHeaderView: FC<CatalogHeaderViewProps> = props =>
 {
     const { node = null } = props;
-    const { currentPage = null, activateNode = null, rootNode = null } = useCatalog();
+    const { currentPage = null, activateNode = null, rootNode = null, getNodeById = null } = useCatalog();
     
     return (
         <>
             <Flex className="catalog-header">
                 <Column gap={ 0 } className="catalog-header-content">
-                    { currentPage && rootNode && <CatalogIconView icon={ node.iconId } /> }
-                    { currentPage && rootNode && <Text bold className="catalog-header-title h-100">{ 'Category Name' }</Text> }
+                    { currentPage && rootNode && <CatalogIconView icon={ getNodeById(currentPage.pageId, rootNode).iconId } /> }
+                    { currentPage && rootNode && <Text bold className="catalog-header-title h-100">{ getNodeById(currentPage.pageId, rootNode).localization }</Text> }
                     <Text className="catalog-header-desc" dangerouslySetInnerHTML={ { __html: currentPage && currentPage.localization.getText(0) } }/>
                 </Column>
                 <Flex className="catalog-header-image" style={ { backgroundImage: `url(${ currentPage && currentPage.localization.getImage(0) })` } } />
