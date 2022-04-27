185e87517e3547334731b4070a9f7c8cfc6fc9ed
robbis95 <82042925+robbis95@users.noreply.github.com>
2022-04-27 14:42:43 +0200

    Seasonal currency colors

--- src/components/purse/views/SeasonalView.tsx
+++ src/components/purse/views/SeasonalView.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { LocalizeFormattedNumber, LocalizeText } from '../../../api';
+import { GetConfiguration, LocalizeFormattedNumber, LocalizeText } from '../../../api';
 import { Flex, LayoutCurrencyIcon, Text } from '../../../common';
 
 interface SeasonalViewProps
@@ -13,7 +13,7 @@ export const SeasonalView: FC<SeasonalViewProps> = props =>
     const { type = -1, amount = -1 } = props;
 
     return (
-        <Flex fullWidth justifyContent="between" className="nitro-purse-seasonal-currency nitro-notification position-relative">
+        <Flex fullWidth justifyContent="between" className={ 'nitro-purse-seasonal-currency nitro-notification position-relative ' + GetConfiguration<boolean>('currency.seasonal.color') }>
             <Flex fullWidth>
                 <Text bold truncate fullWidth className="seasonal-padding seasonal-bold">{ LocalizeText(`purse.seasonal.currency.${ type }`) }</Text>
                 <Text bold variant="white" className="seasonal-padding">{ LocalizeFormattedNumber(amount) }</Text>
