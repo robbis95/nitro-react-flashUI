4ab0dbc167cfedb609b985c4b0d9055dd54116a9
robbis95 <82042925+robbis95@users.noreply.github.com>
2022-04-28 16:01:40 +0200

    Seasonal currency css fix

--- src/components/purse/views/SeasonalView.tsx
+++ src/components/purse/views/SeasonalView.tsx
@@ -13,10 +13,10 @@ export const SeasonalView: FC<SeasonalViewProps> = props =>
     const { type = -1, amount = -1 } = props;
 
     return (
-        <Flex fullWidth justifyContent="between" className={ 'nitro-purse-seasonal-currency nitro-notification position-relative ' + GetConfiguration<boolean>('currency.seasonal.color') }>
+        <Flex fullWidth justifyContent="between" className={ 'nitro-purse-seasonal-currency nitro-notification ' + GetConfiguration<boolean>('currency.seasonal.color') }>
             <Flex fullWidth>
                 <Text bold truncate fullWidth className="seasonal-padding seasonal-bold">{ LocalizeText(`purse.seasonal.currency.${ type }`) }</Text>
-                <Text bold variant="white" className="seasonal-padding">{ LocalizeFormattedNumber(amount) }</Text>
+                <Text bold truncate variant="white" className="seasonal-amount text-end">{ LocalizeFormattedNumber(amount) }</Text>
                 <Flex className="nitro-seasonal-box seasonal-padding">
                     <LayoutCurrencyIcon type={ type } />
                 </Flex>        
