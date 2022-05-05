e5b4ec2ad3b3c831e665a39b62b4f7f69dcbcafb
robbis95 <82042925+robbis95@users.noreply.github.com>
2022-05-05 11:24:48 +0200

    Minor css fixes

--- src/components/notification-center/views/alert-layouts/NitroSystemAlertView.tsx
+++ src/components/notification-center/views/alert-layouts/NitroSystemAlertView.tsx
@@ -33,6 +33,22 @@ export const NitroSystemAlertView: FC<NotificationDefaultAlertViewProps> = props
                         </Column>
                     </Column>
                 </Column>
+                <Column center size={ 5 }>
+                    <Column className="flashui-logo"/>
+                </Column>
+                <Column size={ 7 }>
+                    <Column alignItems="center" gap={ 0 }>
+                        <Text bold fontSize={ 4 }>Flash UI-edit</Text>
+                        <Text small>is created by Robbis</Text>
+                    </Column>
+                    <Column fullWidth gap={ 1 }>
+                        <Button fullWidth variant="success" onClick={ event => window.open('https://discord.gg/j3bqtZKAF3') }>Flash-UI Discord</Button>
+                        <Flex gap={ 1 }>
+                            <Button fullWidth onClick={ event => window.open('https://git.krews.org/robbis1111/nitro-react-flashUI') }>Git</Button>
+                            <Button fullWidth onClick={ event => window.open('https://git.krews.org/robbis1111/nitro-react-flashUI/-/issues') }>Bug Report</Button>
+                        </Flex>
+                    </Column>
+                </Column>
             </Grid>
         </LayoutNotificationAlertView>
     );
