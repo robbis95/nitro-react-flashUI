f0a0489eb080301cd7b4fbe6ec7a21d12ac11520
robbis95 <82042925+robbis95@users.noreply.github.com>
2023-05-04 22:27:50 +0200

    improved colourizer code

--- src/api/utils/Colourizer.ts
+++ src/api/utils/Colourizer.ts
@@ -1,18 +1,17 @@
-const cache: Map<string, string> = new Map();
+const cache = new Map<string, string>();
+const canvasEl = document.createElement('canvas');
 
-const canvasEl: HTMLCanvasElement = document.createElement('canvas');
-
-export async function tintImage(image: string, hex: string, opacity: number = 0, secondary: string = '#000')
+export async function tintImage(imageSrc: string, hex: string, opacity = 0, secondary = '#000') 
 {
-    let namespace = `${ image }-${ hex }-${ secondary }-${ opacity }`;
+    const cacheKey = `${ imageSrc }-${ hex }-${ secondary }-${ opacity }`;
 
-    if (cache.get(namespace))
+    if (cache.has(cacheKey)) 
     {
-        return cache.get(namespace);
+        return cache.get(cacheKey);
     }
 
     const img = new Image();
-    img.src = image;
+    img.src = imageSrc;
 
     await new Promise((resolve, reject) => 
     {
@@ -23,103 +22,105 @@ export async function tintImage(image: string, hex: string, opacity: number = 0,
     return tint(img, hex, opacity, secondary);
 }
 
-function colourPixel(data: Uint8ClampedArray, i: number, rgb: { r,g,b }, opacity: number) 
+function colorPixel(data: Uint8ClampedArray, index: number, rgb: { r, g, b }, opacity: number) 
 {
     if (!rgb?.r || !rgb?.g || !rgb?.b) return;
 
-    data[i] = rgb.r - (data[i] < 255 ? (255 - data[i]) : 0);
-    data[i + 1] = rgb.g - (data[i + 1] < 255 ? (255 - data[i + 1]) : 0);
-    data[i + 2] = rgb.b - (data[i + 2] < 255 ? (255 - data[i + 2]) : 0);
-    data[i + 3] = 255 - opacity;
+    data[index] = rgb.r - (data[index] < 255 ? (255 - data[index]) : 0);
+    data[index + 1] = rgb.g - (data[index + 1] < 255 ? (255 - data[index + 1]) : 0);
+    data[index + 2] = rgb.b - (data[index + 2] < 255 ? (255 - data[index + 2]) : 0);
+    data[index + 3] = 255 - opacity;
 }
 
-export function tint(image: HTMLImageElement, hex: string, opacity: number = 0, secondary: string = '#000')
+export function tint(image: HTMLImageElement, hex: string, opacity = 0, secondary = '#000') 
 {
-    let namespace = `${ image.src }-${ hex }-${ secondary }-${ opacity }`;
+    const cacheKey = `${ image.src }-${ hex }-${ secondary }-${ opacity }`;
 
-    const ctx = canvasEl.getContext('2d',{ willReadFrequently: true });
+    const ctx = canvasEl.getContext('2d', { willReadFrequently: true });
 
-    if (cache.get(namespace))
+    if (cache.has(cacheKey)) 
     {
-        return cache.get(namespace);
+        return cache.get(cacheKey);
     }
-    
+
     canvasEl.width = image.width;
     canvasEl.height = image.height;
 
     ctx.drawImage(image, 0, 0);
 
-    const data = ctx.getImageData(0, 0, canvasEl.width, canvasEl.height,{});
+    const imageData = ctx.getImageData(0, 0, canvasEl.width, canvasEl.height, {});
 
-    let rgb = hexToRgb(hex);
-    const rgb2 = hexToRgb(secondary);
+    const mainRgb = hexToRgb(hex);
+    const secondaryRgb = hexToRgb(secondary);
 
-    if(!rgb) return canvasEl.toDataURL();
+    if (!mainRgb) return canvasEl.toDataURL();
 
-    for(let i = 0; i < data.data.length; i += 4) 
+    for (let i = 0; i < imageData.data.length; i += 4) 
     {
-        if(data.data[i] + data.data[i + 1] + data.data[i + 2] + data.data[i + 2] !== 0) 
+        if (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2] !== 0) 
         {
-
-            if(secondary !== '#000' && data.data[i] <= (255 * 2) / 3) 
+            if (secondary !== '#000' && imageData.data[i] <= (255 * 2) / 3) 
             {
-                data.data[i] = (data.data[i] + 127)
-                data.data[i + 1] = (data.data[i + 1] + 127)
-                data.data[i + 2] = (data.data[i + 2] + 127) 
+                imageData.data[i] += 127;
+                imageData.data[i + 1] += 127;
+                imageData.data[i + 2] += 127;
 
-                colourPixel(data.data,i,rgb2,opacity);
+                colorPixel(imageData.data, i, secondaryRgb, opacity);
             }
-            else colourPixel(data.data,i,rgb,opacity);
-
+            else 
+            {
+                colorPixel(imageData.data, i, mainRgb, opacity);
+            }
         }
         else 
         {
-            if(data.data[i + 3] != 0 && opacity) data.data[i + 3] = 255 - opacity;
+            if (imageData.data[i + 3] !== 0 && opacity) imageData.data[i + 3] = 255 - opacity;
         }
     }
 
-    ctx.putImageData(data, 0, 0);
+    ctx.putImageData(imageData, 0, 0);
 
-    cache.set(namespace, canvasEl.toDataURL())
+    cache.set(cacheKey, canvasEl.toDataURL());
 
-    return cache.get(namespace);
+    return cache.get(cacheKey);
 }
 
-function hexToRgb(hex) 
+function hexToRgb(hex: string) 
 {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
         r: parseInt(result[1], 16),
         g: parseInt(result[2], 16),
         b: parseInt(result[3], 16)
     } : null;
 }
-
-function componentToHex(c) 
+  
+function componentToHex(c: number) 
 {
-    var hex = c.toString(16);
-    return hex.length == 1 ? '0' + hex : hex;
+    const hex = c.toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
 }
-
-export function rgbToHex(r, g, b) 
+  
+export function rgbToHex(r: number, g: number, b: number) 
 {
-    return '#' + componentToHex(parseInt(r)) + componentToHex(parseInt(g)) + componentToHex(parseInt(b));
+    return '#' + componentToHex(parseInt(String(r))) + componentToHex(parseInt(String(g))) + componentToHex(parseInt(String(b)));
 }
-
+  
 export function getStyleOf(selector: string, property: string) 
 {
     let result = '';
-
+  
     for (const sheet of Array.from(document.styleSheets)) 
     {
         for (const rule of Array.from(sheet.cssRules || sheet.rules)) 
         {
-            if (rule instanceof CSSStyleRule && rule.selectorText == selector) 
+            if (rule instanceof CSSStyleRule && rule.selectorText === selector) 
             {
                 result = rule.style[property];
             }
         }
     }
-
+  
     return result;
 }
+  
