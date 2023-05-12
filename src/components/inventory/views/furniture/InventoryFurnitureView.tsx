a468af4222ae414faf0c4848a47a18ff66e6f189
oobjectt <objectholos@gmail.com>
2023-05-12 22:12:56 +0200

    Feature - Trading

--- src/components/inventory/views/furniture/InventoryFurnitureView.tsx
+++ src/components/inventory/views/furniture/InventoryFurnitureView.tsx
@@ -1,17 +1,19 @@
-import { IRoomSession, RoomObjectVariable, RoomPreviewer, Vector3d } from '@nitrots/nitro-renderer';
+import { IObjectData, IRoomSession, RoomObjectVariable, RoomPreviewer, TradingListAddItemComposer, TradingListAddItemsComposer, Vector3d } from '@nitrots/nitro-renderer';
 import { FC, useEffect, useState } from 'react';
-import { attemptItemPlacement, DispatchUiEvent, FurniCategory, GetRoomEngine, GetSessionDataManager, GroupItem, LocalizeText, UnseenItemCategory } from '../../../../api';
+import { DispatchUiEvent, FurniCategory, GetRoomEngine, GetSessionDataManager, GroupItem, IFurnitureItem, LocalizeText, NotificationAlertType, SendMessageComposer, UnseenItemCategory, attemptItemPlacement, getGuildFurniType } from '../../../../api';
 import { AutoGrid, Button, Column, Grid, LayoutLimitedEditionCompactPlateView, LayoutRarityLevelView, LayoutRoomPreviewerView, Text } from '../../../../common';
 import { CatalogPostMarketplaceOfferEvent } from '../../../../events';
-import { useInventoryFurni, useInventoryUnseenTracker } from '../../../../hooks';
+import { useInventoryFurni, useInventoryTrade, useInventoryUnseenTracker, useNotification } from '../../../../hooks';
 import { InventoryCategoryEmptyView } from '../InventoryCategoryEmptyView';
 import { InventoryFurnitureItemView } from './InventoryFurnitureItemView';
 import { InventoryFurnitureSearchView } from './InventoryFurnitureSearchView';
+import { MAX_ITEMS_TO_TRADE } from './InventoryTradeView';
 
 interface InventoryFurnitureViewProps
 {
     roomSession: IRoomSession;
     roomPreviewer: RoomPreviewer;
+    isTrading?: boolean;
 }
 
 const attemptPlaceMarketplaceOffer = (groupItem: GroupItem) =>
@@ -27,11 +29,121 @@ const attemptPlaceMarketplaceOffer = (groupItem: GroupItem) =>
 
 export const InventoryFurnitureView: FC<InventoryFurnitureViewProps> = props =>
 {
-    const { roomSession = null, roomPreviewer = null } = props;
+    const { roomSession = null, roomPreviewer = null, isTrading = null } = props;
     const [ isVisible, setIsVisible ] = useState(false);
     const [ filteredGroupItems, setFilteredGroupItems ] = useState<GroupItem[]>([]);
+    const [ groupItem, setGroupItem ] = useState<GroupItem>(null);
+    const [ quantity, setQuantity ] = useState<number>(1);
     const { groupItems = [], selectedItem = null, activate = null, deactivate = null } = useInventoryFurni();
+    const { ownUser = null } = useInventoryTrade();
     const { resetItems = null } = useInventoryUnseenTracker();
+    const { simpleAlert = null } = useNotification();
+
+    const updateQuantity = (value: number, totalItemCount: number) =>
+    {
+        if(isNaN(Number(value)) || Number(value) < 0 || !value) value = 1;
+
+        value = Math.max(Number(value), 1);
+        value = Math.min(Number(value), totalItemCount);
+
+        if(value === quantity) return;
+
+        setQuantity(value);
+    }
+
+    const changeCount = (totalItemCount: number) =>
+    {
+        updateQuantity(quantity, totalItemCount);
+        attemptItemOffer(quantity);
+    }
+
+    const canTradeItem = (isWallItem: boolean, spriteId: number, category: number, groupable: boolean, stuffData: IObjectData) =>
+    {
+        if(!ownUser || ownUser.accepts || !ownUser.userItems) return false;
+
+        if(ownUser.userItems.length < MAX_ITEMS_TO_TRADE) return true;
+
+        if(!groupable) return false;
+
+        let type = spriteId.toString();
+
+        if(category === FurniCategory.POSTER)
+        {
+            type = ((type + 'poster') + stuffData.getLegacyString());
+        }
+        else
+        {
+            if(category === FurniCategory.GUILD_FURNI)
+            {
+                type = getGuildFurniType(spriteId, stuffData);
+            }
+            else
+            {
+                type = (((isWallItem) ? 'I' : 'S') + type);
+            }
+        }
+
+        return !!ownUser.userItems.getValue(type);
+    }
+
+    const attemptItemOffer = (count: number) =>
+    {
+        console.log('item', groupItem);
+        
+        if(!groupItem) return;
+
+        const tradeItems = groupItem.getTradeItems(count);
+
+        if(!tradeItems || !tradeItems.length) return;
+
+        let coreItem: IFurnitureItem = null;
+        const itemIds: number[] = [];
+
+        for(const item of tradeItems)
+        {
+            itemIds.push(item.id);
+
+            if(!coreItem) coreItem = item;
+        }
+
+        const ownItemCount = ownUser.userItems.length;
+
+        if((ownItemCount + itemIds.length) <= 1500)
+        {
+            if(!coreItem.isGroupable && (itemIds.length))
+            {
+                SendMessageComposer(new TradingListAddItemComposer(itemIds.pop()));
+            }
+            else
+            {
+                const tradeIds: number[] = [];
+
+                for(const itemId of itemIds)
+                {
+                    if(canTradeItem(coreItem.isWallItem, coreItem.type, coreItem.category, coreItem.isGroupable, coreItem.stuffData))
+                    {
+                        tradeIds.push(itemId);
+                    }
+                }
+
+                if(tradeIds.length)
+                {
+                    if(tradeIds.length === 1)
+                    {
+                        SendMessageComposer(new TradingListAddItemComposer(tradeIds.pop()));
+                    }
+                    else
+                    {
+                        SendMessageComposer(new TradingListAddItemsComposer(...tradeIds));
+                    }
+                }
+            }
+        }
+        else
+        {
+            simpleAlert(LocalizeText('trading.items.too_many_items.desc'), NotificationAlertType.DEFAULT, null, null, LocalizeText('trading.items.too_many_items.title'));
+        }
+    }
 
     useEffect(() =>
     {
@@ -81,6 +193,8 @@ export const InventoryFurnitureView: FC<InventoryFurnitureViewProps> = props =>
                 roomPreviewer.addFurnitureIntoRoom(selectedItem.type, new Vector3d(90), selectedItem.stuffData, (furnitureItem.extra.toString()));
             }
         }
+
+        setGroupItem(selectedItem);
     }, [ roomPreviewer, selectedItem ]);
 
     useEffect(() =>
@@ -115,7 +229,7 @@ export const InventoryFurnitureView: FC<InventoryFurnitureViewProps> = props =>
             <Column size={ 7 } overflow="hidden">
                 <InventoryFurnitureSearchView groupItems={ groupItems } setGroupItems={ setFilteredGroupItems } />
                 <AutoGrid columnCount={ 5 }>
-                    { filteredGroupItems && (filteredGroupItems.length > 0) && filteredGroupItems.map((item, index) => <InventoryFurnitureItemView key={ index } groupItem={ item } />) }
+                    { filteredGroupItems && (filteredGroupItems.length > 0) && filteredGroupItems.map((item, index) => <InventoryFurnitureItemView key={ index } groupItem={ item } isTrading={ isTrading } attemptItemOffer={ (e) => attemptItemOffer(e) } setGroupItem={ (e) => setGroupItem(e) } />) }
                 </AutoGrid>
             </Column>
             <Column size={ 5 } overflow="auto">
@@ -128,16 +242,26 @@ export const InventoryFurnitureView: FC<InventoryFurnitureViewProps> = props =>
                 </Column>
                 { selectedItem &&
                     <Column grow justifyContent="between" gap={ 2 }>
-                        <Text grow truncate>{ selectedItem.name }</Text>
+                        <Text grow truncate bold>{ selectedItem.name }</Text>
                         <Column gap={ 1 }>
-                            { !!roomSession &&
-                                <Button onClick={ event => attemptItemPlacement(selectedItem) }>
-                                    { LocalizeText('inventory.furni.placetoroom') }
-                                </Button> }
-                            { (selectedItem && selectedItem.isSellable) &&
-                                <Button onClick={ event => attemptPlaceMarketplaceOffer(selectedItem) }>
-                                    { LocalizeText('inventory.marketplace.sell') }
-                                </Button> }
+                            { (!isTrading) &&
+                                <>
+                                    { !!roomSession &&
+                                        <Button onClick={ event => attemptItemPlacement(selectedItem) }>
+                                            { LocalizeText('inventory.furni.placetoroom') }
+                                        </Button> }
+                                    { (selectedItem && selectedItem.isSellable) &&
+                                        <Button onClick={ event => attemptPlaceMarketplaceOffer(selectedItem) }>
+                                            { LocalizeText('inventory.marketplace.sell') }
+                                        </Button> }
+                                </>
+                            }
+                            { (isTrading) &&
+                                <Column gap={ 1 } alignItems="start">
+                                    <input type="number" className="w-50 quantity-input remove-outline" placeholder={ LocalizeText('catalog.bundlewidget.spinner.select.amount') } disabled={ selectedItem.getUnlockedCount() === 0 } value={ quantity } onChange={ event => setQuantity(event.target.valueAsNumber) } />
+                                    <Button variant="secondary" disabled={ selectedItem.getUnlockedCount() === 0 } onClick={ event => changeCount(selectedItem.getUnlockedCount()) }>{ LocalizeText('inventory.trading.areoffering') }</Button>
+                                </Column>
+                            }
                         </Column>
                     </Column> }
             </Column>
