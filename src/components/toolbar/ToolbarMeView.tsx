b4f01caf623cb428f74caa5a3e7121bd10a01b22
Gizmo <77913129+Habbobba@users.noreply.github.com>
2023-05-21 06:05:11 -0400

    Update ToolbarMeView.tsx
    Added text color changing hovering effect to the tool bar icons. 
    The text now turns a blue color when you hover an icon on the tool bar menu.

--- src/components/toolbar/ToolbarMeView.tsx
+++ src/components/toolbar/ToolbarMeView.tsx
@@ -7,19 +7,22 @@ import { GuideToolEvent } from '../../events';
 interface ToolbarMeViewProps
 {
     useGuideTool: boolean;
+	
     unseenAchievementCount: number;
+	
     setMeExpanded: Dispatch<SetStateAction<boolean>>;
 }
 
 export const ToolbarMeView: FC<PropsWithChildren<ToolbarMeViewProps>> = props =>
 {
     const { useGuideTool = false, unseenAchievementCount = 0, setMeExpanded = null, children = null, ...rest } = props;
+	
     const elementRef = useRef<HTMLDivElement>();
 
     useEffect(() =>
     {
         const roomSession = GetRoomSession();
-
+		
         if(!roomSession) return;
 
         GetRoomEngine().selectRoomObject(roomSession.roomId, roomSession.ownRoomIndex, RoomObjectCategory.UNIT);
@@ -35,30 +38,48 @@ export const ToolbarMeView: FC<PropsWithChildren<ToolbarMeViewProps>> = props =>
     }, [ setMeExpanded ]);
 
     return (
-        <Flex innerRef={ elementRef } alignItems="center" className="nitro-toolbar-me p-2" gap={ 3 }>
-            { (GetConfiguration('guides.enabled') && useGuideTool) &&
-                <Column gap={ 1 } alignItems="center" onClick={ event => DispatchUiEvent(new GuideToolEvent(GuideToolEvent.TOGGLE_GUIDE_TOOL)) }>
-                    <Base pointer className="navigation-item icon icon-me-helper-tool" />
-                    <Text className="me-text">{ LocalizeText('widget.memenu.guide') }</Text>
-                </Column> }
-            <Column gap={ 1 } alignItems="center" onClick={ event => CreateLinkEvent('achievements/toggle') }>
+		<Flex innerRef={ elementRef } alignItems="center" className="nitro-toolbar-me p-2" gap={ 3 }>
+			{ (GetConfiguration('guides.enabled') && useGuideTool) &&
+				<Column className="me-text" gap={ 1 } alignItems="center" onClick={ event => DispatchUiEvent(new GuideToolEvent(GuideToolEvent.TOGGLE_GUIDE_TOOL)) }>
+					<Base pointer className="navigation-item icon icon-me-helper-tool" />
+					<Text>
+						{ LocalizeText('widget.memenu.guide') }
+					</Text>
+				</Column> 
+			}
+            <Column className="me-text" gap={ 1 } alignItems="center" onClick={ event => CreateLinkEvent('achievements/toggle') }>
                 <Base pointer className="navigation-item icon icon-me-achievements">
                     { (unseenAchievementCount > 0) &&
-                    <LayoutItemCountView count={ unseenAchievementCount } /> }
+						<LayoutItemCountView count={ unseenAchievementCount } />
+					}
                 </Base>
-                <Text className="me-text">{ LocalizeText('widget.memenu.achievements') }</Text>
+                <Text>
+					{ LocalizeText('widget.memenu.achievements') }
+				</Text>
             </Column>
-            <Column gap={ 1 } alignItems="center" onClick={ event => GetUserProfile(GetSessionDataManager().userId) }>
+            <Column className="me-text" gap={ 1 } alignItems="center" onClick={ event => GetUserProfile(GetSessionDataManager().userId) }>
                 <Base pointer className="navigation-item icon icon-me-profile" />
-                <Text className="me-text">{ LocalizeText('widget.memenu.profile') }</Text>
+                <Text>
+					{ LocalizeText('widget.memenu.profile') }
+				</Text>
             </Column>
-            <Column gap={ 1 } alignItems="center" onClick={ event => CreateLinkEvent('navigator/search/myworld_view') }>
+            <Column className="me-text" gap={ 1 } alignItems="center" onClick={ event => CreateLinkEvent('navigator/search/myworld_view') }>
                 <Base pointer className="navigation-item icon icon-me-rooms" />
-                <Text className="me-text">{ LocalizeText('widget.memenu.myrooms') }</Text>
+                <Text>
+					{ LocalizeText('widget.memenu.myrooms') }
+				</Text>
             </Column>
-            <Column gap={ 1 } alignItems="center" onClick={ event => CreateLinkEvent('avatar-editor/toggle') }>
+            <Column className="me-text" gap={ 1 } alignItems="center" onClick={ event => CreateLinkEvent('avatar-editor/toggle') }>
                 <Base pointer className="navigation-item icon icon-me-clothing" />
-                <Text className="me-text">{ LocalizeText('widget.memenu.myclothes') }</Text>
+                <Text>
+					{ LocalizeText('widget.memenu.myclothes') }
+				</Text>
+            </Column>
+            <Column className="me-text" gap={ 1 } alignItems="center" onClick={ event => CreateLinkEvent('forums/toggle') }>
+                <Base pointer className="navigation-item icon icon-me-forums" />
+                <Text>
+					{ LocalizeText('widget.memenu.forums') }
+				</Text>
             </Column>
             { children }
         </Flex>
