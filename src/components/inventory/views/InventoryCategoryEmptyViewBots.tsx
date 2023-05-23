2f228fee7816283106c7175aa61a3be674e48899
duckietm <troop@mailtjes.com>
2023-05-23 19:31:57 +0200

    Added: Empty Bot & Pet Screen

---
+++ src/components/inventory/views/InventoryCategoryEmptyViewBots.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import { Column, Grid, GridProps, Text } from "../../../common";
+
+export interface InventoryCategoryEmptyViewBotsProps extends GridProps {
+  title: string;
+  desc: string;
+}
+
+export const InventoryCategoryEmptyViewBots: FC<
+  InventoryCategoryEmptyViewBotsProps
+> = (props) => {
+  const { title = "", desc = "", children = null, ...rest } = props;
+
+  return (
+    <Grid {...rest}>
+      <Column justifyContent="start" center size={6} overflow="hidden">
+        <div className="empty-botsimage" />
+      </Column>
+      <Column justifyContent="center" size={6} overflow="hidden">
+        <div className="bubble-inventory bubble-inventory-bottom-left">
+          <Text bold fontSize={6} overflow="unset" truncate>
+            {title}
+          </Text>
+          <Text overflow="auto" fontSize={6}>
+            {" "}
+            {desc}
+          </Text>
+        </div>
+      </Column>
+      {children}
+    </Grid>
+  );
+};
---
+++ src/components/inventory/views/InventoryCategoryEmptyViewPets.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import { Column, Grid, GridProps, Text } from "../../../common";
+
+export interface InventoryCategoryEmptyViewPetsProps extends GridProps {
+  title: string;
+  desc: string;
+}
+
+export const InventoryCategoryEmptyViewPets: FC<
+  InventoryCategoryEmptyViewPetsProps
+> = (props) => {
+  const { title = "", desc = "", children = null, ...rest } = props;
+
+  return (
+    <Grid {...rest}>
+      <Column justifyContent="start" center size={6} overflow="hidden">
+        <div className="empty-petsimage" />
+      </Column>
+      <Column justifyContent="center" size={6} overflow="hidden">
+        <div className="bubble-inventory bubble-inventory-bottom-left">
+          <Text bold fontSize={6} overflow="unset" truncate>
+            {title}
+          </Text>
+          <Text overflow="auto" fontSize={6}>
+            {" "}
+            {desc}
+          </Text>
+        </div>
+      </Column>
+      {children}
+    </Grid>
+  );
+};
