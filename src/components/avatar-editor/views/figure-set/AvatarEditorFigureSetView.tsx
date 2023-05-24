1529694ae090a7e0f20b7507b249c46d44ef987b
Ente Robert (YIODC) <bhc89@eur.corp.vattenfall.com>
2023-05-24 08:46:26 +0200

    Addon: Footbal Gate (done by Object)

--- src/components/avatar-editor/views/figure-set/AvatarEditorFigureSetView.tsx
+++ src/components/avatar-editor/views/figure-set/AvatarEditorFigureSetView.tsx
@@ -1,8 +1,12 @@
-import { HabboClubLevelEnum } from '@nitrots/nitro-renderer';
 import { Dispatch, FC, SetStateAction, useCallback, useEffect, useRef } from 'react';
 import { AvatarEditorGridPartItem, CategoryData, CreateLinkEvent, GetSessionDataManager, IAvatarEditorCategoryModel } from '../../../../api';
 import { AutoGrid } from '../../../../common';
 import { AvatarEditorFigureSetItemView } from './AvatarEditorFigureSetItemView';
+
+const TSHIRT_FOOTBALL_GATE = [ 3111, 3110, 3109, 3030, 3114, 266, 265, 262, 3113, 3112, 691, 690, 667 ];
+const NUMBER_BEHIND_FOOTBALL_GATE = [ 3128, 3127, 3126, 3125, 3124, 3123, 3122, 3121, 3120, 3119 ];
+const PANTS_FOOTBALL_GATE = [ 3116, 281, 275, 715, 700, 696, 3006 ];
+const SHOES_FOOTBALL_GATE = [ 3115, 3068, 906 ];
 
 export interface AvatarEditorFigureSetViewProps
 {
@@ -13,7 +17,7 @@ export interface AvatarEditorFigureSetViewProps
 
 export const AvatarEditorFigureSetView: FC<AvatarEditorFigureSetViewProps> = props =>
 {
-    const { model = null, category = null, setMaxPaletteCount = null } = props;
+    const { model = null, category = null, isFromFootballGate = false, setMaxPaletteCount = null } = props;
     const elementRef = useRef<HTMLDivElement>(null);
 
     const selectPart = useCallback((item: AvatarEditorGridPartItem) =>
@@ -40,8 +44,10 @@ export const AvatarEditorFigureSetView: FC<AvatarEditorFigureSetViewProps> = pro
 
     return (
         <AutoGrid className="clothing-container" innerRef={ elementRef } columnCount={ 3 } columnMinHeight={ 50 }>
-            { (category.parts.length > 0) && category.parts.map((item, index) =>
-                <AvatarEditorFigureSetItemView key={ index } partItem={ item } onClick={ event => selectPart(item) } />) }
-        </AutoGrid>
+            { (category.parts.length > 0) && category.parts.map(item =>
+                (!isFromFootballGate || (isFromFootballGate && TSHIRT_FOOTBALL_GATE.includes(item.id) || NUMBER_BEHIND_FOOTBALL_GATE.includes(item.id) || PANTS_FOOTBALL_GATE.includes(item.id) || SHOES_FOOTBALL_GATE.includes(item.id))) &&
+                    <AvatarEditorFigureSetItemView key={ item.id } partItem={ item } onClick={ event => selectPart(item) } />)
+            }
+			</AutoGrid>
     );
 }
