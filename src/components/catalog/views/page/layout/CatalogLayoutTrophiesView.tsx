848ac60bf4a1095b3755290a95f252e71cf78765
object <110488133+oobjectt@users.noreply.github.com>
2023-07-01 12:36:52 +0200

    Fixes
    - Fix arrow left pagination
    - The trophy with localizationId empty was not working correctly in color bronze
    - Design tweaks
    - Component optimization

--- src/components/catalog/views/page/layout/CatalogLayoutTrophiesView.tsx
+++ src/components/catalog/views/page/layout/CatalogLayoutTrophiesView.tsx
@@ -1,5 +1,6 @@
-import { FC, useEffect, useRef, useState } from 'react';
-import { AutoGrid, Base, Button, Column, Flex, Text } from '../../../../../common';
+import { FC, useEffect, useState } from 'react';
+import { Base, Button, ButtonGroup, Column, Flex, Text } from '../../../../../common';
+import { GiftColorButton } from '../../../../../common/GiftColorButton';
 import { useCatalog } from '../../../../../hooks';
 import { CatalogPurchaseWidgetView } from '../widgets/CatalogPurchaseWidgetView';
 import { CatalogTotalPriceWidget } from '../widgets/CatalogTotalPriceWidget';
@@ -11,69 +12,56 @@ export const CatalogLayoutTrophiesView: FC<CatalogLayoutProps> = props =>
     const [ trophyText, setTrophyText ] = useState<string>('');
     const { setCurrentOffer = null, setPurchaseOptions = null, currentPage = null } = useCatalog();
     const [ index, setIndex ] = useState(0);
-    const elementRef = useRef<HTMLDivElement>();
     const [ trophies, setTrophies ] = useState([]);
-    const [ qtdOffers, setQtdOffers ] = useState(0)
-    
+    const [ trophyColor, setTrophyColor ] = useState<string>('_g');
+    const [ colors, setColors ] = useState<{ id: string, color: string }[]>([ { id: '_g', color: '#CBA200' }, { id: '_s', color: '#A2A2A2' }, { id: '_b', color: '#A25100' } ]);
+    const [ lastPageTrophies, setLastPageTrophies ] = useState<number>(currentPage.pageId);
+
     useEffect(() =>
-    {  
-        if (currentPage.offers.length !== qtdOffers)
+    {
+        if (currentPage.pageId !== lastPageTrophies && currentPage.layoutCode === 'trophies')
         {
-            setTrophies([]);
             setIndex(0);
+            setTrophies([]);
+            setTrophyColor('_g');
         }
-        if(trophies.length === 0) 
-        {
-            // Set the total offers from trophies catalog
-            setQtdOffers(currentPage.offers.length)
-            // Default : Gold Trophie and uniques (Staff)
-            const trophies = currentPage.offers.filter((trophy) => trophy.localizationId.includes('_g') || !trophy.localizationId.match(/[_gsb]/));
-            setCurrentOffer(trophies[index]);
-            setTrophies(trophies)
-        } 
+        
+        const trophies = currentPage.offers.filter( trophy => (trophyColor !== '_b' ? trophy.localizationId.endsWith(trophyColor) : trophy.localizationId.endsWith(trophyColor) || trophy.localizationId.endsWith(trophy.localizationId.charAt(trophy.localizationId.length - 2)) ) || (trophy.localizationId.charAt(trophy.localizationId.length - 2) !== '_' && trophy.localizationId !== '') );
+        
+        setTrophies(trophies)
+        setCurrentOffer(trophies[index]);
+        setLastPageTrophies(currentPage.pageId);
+         
         setPurchaseOptions(prevValue =>
         {
             const newValue = { ...prevValue };
             newValue.extraData = trophyText;
             return newValue;
         });
-    }, [ trophies, index, currentPage, trophyText, qtdOffers, setCurrentOffer, setPurchaseOptions, setQtdOffers ]);
+    }, [ trophyText, setPurchaseOptions, currentPage.offers, setCurrentOffer, index, trophyColor, currentPage.pageId, currentPage.layoutCode, lastPageTrophies ]);
 
-
-    const setTrophyColor = (color: string) =>  
+    const nextOffer = () =>
     {
-        //Set color to the selected Trophie (_g = gold, _s = silver, _b = bronze)
-        const trophies = currentPage.offers.filter((trophy) => trophy.localizationId.includes(color));
-        setTrophies(trophies)
-        setCurrentOffer(trophies[index]);
+        setIndex(prevValue =>
+        {
+            let newIndex = (prevValue + 1);
+
+            if(newIndex >= trophies.length) newIndex = 0;
+
+            return newIndex;
+        });
     }
 
     const lastOffer = () =>
     {
-        setIndex(prevIndex => prevIndex - 1);
-        if(index <= 0)
+        setIndex(prevValue =>
         {
-            setIndex(0);
-            setCurrentOffer(trophies[index]);
-        }
-        else 
-        {
-            setCurrentOffer(trophies[index - 1]);
-        }
-    }
+            let newIndex = (prevValue - 1);
 
-    const nextOffer = () =>
-    {
-        setIndex(prevIndex => prevIndex + 1);
-        if(index >= trophies.length - 1) 
-        {
-            setIndex(0);
-            setCurrentOffer(trophies[0]);
-        }
-        else 
-        {
-            setCurrentOffer(trophies[index + 1]);
-        }
+            if(newIndex < 0) newIndex = (trophies.length - 1);
+
+            return newIndex;
+        });
     }
 
     return (
@@ -82,42 +70,25 @@ export const CatalogLayoutTrophiesView: FC<CatalogLayoutProps> = props =>
                 <Text dangerouslySetInnerHTML={ { __html: currentPage && currentPage.localization.getText(1) } } style={ { fontSize: '12px' } }></Text>
                 <Column fit overflow="hidden" className="p-2 rounded d-flex justify-content-center align-items-center">
                     <div className="position-absolute bottom-0" style={ { left: '105px' } }>
-                        <Button variant="primary" className="volter-button btn-sm" onClick={ lastOffer }>
+                        <Button variant="primary" className="btn btn-button btn-sm" onClick={ lastOffer }>
                             <Base className="flex-shrink-0 icon icon-chevron_left_gift" />
                         </Button>
                     </div>
-                    <div className="position-absolute bottom-0" style={ { top: '150px' } }>
+                    <div className="position-absolute bottom-3">
                         <CatalogViewTrophiesWidgetView />
                     </div>
                     <div className="position-absolute bottom-0" style={ { right: '105px' } }>
-                        <Button variant="primary" className="volter-button btn-sm" onClick={ nextOffer }>
+                        <Button variant="primary" className="btn btn-button btn-sm" onClick={ nextOffer }>
                             <Base className="flex-shrink-0 icon icon-chevron_right_gift" />
                         </Button>
                     </div>
                 </Column>
-                <CatalogTotalPriceWidget className="credits-default-layout credits-bg py-1 px-2 bottom-1 end-1" justifyContent="end" alignItems="end" />
+                <CatalogTotalPriceWidget className="credits-default-layout credits-bg py-1 px-2 bottom-0 end-0" justifyContent="end" alignItems="end" />
             </Column>
-            <Column className="grid-bg w-10 h-10 p-2 mt-2 flex" size={ 3 } overflow="hidden" gap={ 2 }>
-                <AutoGrid innerRef={ elementRef } columnCount={ 5 } >
-                    <Column className="catalog-grid-active cursor-pointer" gap={ 0 }>
-                        <Base fit className="unique-bg-override" />
-                        <div className="border border-2 border-white" style={ { backgroundColor: '#CBA200' } } onClick={ () => setTrophyColor('_g') } >
-                            <i className="mr-1 d-inline-block outline" />
-                        </div>
-                    </Column>
-                    <Column className="catalog-grid-active cursor-pointer" gap={ 0 }>
-                        <Base fit className="unique-bg-override" />
-                        <div className="border border-2 border-white" style={ { backgroundColor: '#A2A2A2' } } onClick={ () => setTrophyColor('_s') } >
-                            <i className="mr-1 d-inline-block outline" />
-                        </div>
-                    </Column>
-                    <Column className="catalog-grid-active cursor-pointer" gap={ 0 }>
-                        <Base fit className="unique-bg-override"/>
-                        <div className="border border-2 border-white" style={ { backgroundColor: '#A25100' } } onClick={ () => setTrophyColor('_b') } >
-                            <i className="mr-1 d-inline-block outline" />
-                        </div>
-                    </Column>
-                </AutoGrid>
+            <Column className="grid-bg p-2 mt-2" overflow="hidden">
+                <ButtonGroup fullWidth>
+                    { colors.map(color => <GiftColorButton key={ color.id } variant="dark" className="color-trophy cursor-pointer" active={ (color.id === trophyColor) } style={ { backgroundColor: color.color, borderRadius: '4px' } } onClick={ () => setTrophyColor(color.id) } />) }
+                </ButtonGroup>
             </Column>
             <Column size={ 3 } overflow="hidden">
                 <textarea className="flex-grow-1 form-control w-40 trophy-text mt-2" defaultValue={ trophyText || '' } onChange={ event => setTrophyText(event.target.value) } />
