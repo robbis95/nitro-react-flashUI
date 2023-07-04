fd5c03d74f0d49ae73b54d7193144390da64f355
object <110488133+oobjectt@users.noreply.github.com>
2023-07-04 20:49:47 +0200

    Inventory - Fixed badges scroll & alignment badges in infostand

--- src/components/inventory/InventoryView.tsx
+++ src/components/inventory/InventoryView.tsx
@@ -112,7 +112,7 @@ export const InventoryView: FC<{}> = props =>
     if(!isVisible) return null;
 
     return (
-        <NitroCardView uniqueKey={ 'inventory' } className={ `${ isTrading ? 'nitro-inventory trading  no-resize' : 'nitro-inventory' }` } theme={ isTrading ? 'primary' : '' } >
+        <NitroCardView uniqueKey={ 'inventory' } className={ `${ isTrading ? 'nitro-inventory trading no-resize' : 'nitro-inventory' }` } theme={ isTrading ? 'primary' : '' } style={ { width: isTrading ? '490px' : '4px' } }>
             <NitroCardHeaderView headerText={ LocalizeText('inventory.title') } onCloseClick={ onClose } />
             <>
                 <NitroCardTabsView>
