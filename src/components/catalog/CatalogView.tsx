7bf5126a1ded36eb1688fc0f569864693b66336f
duckietm <troop@mailtjes.com>
2023-08-08 11:43:46 +0200

    Update CatalogView
    Fix the following:
    
    Warning: Encountered two children with the same key, -1. Keys should be unique so that components maintain their identity across updates. Non-unique keys may cause children to be duplicated and/or omitted — the behavior is unsupported and could change in a future version.

--- src/components/catalog/CatalogView.tsx
+++ src/components/catalog/CatalogView.tsx
@@ -10,21 +10,17 @@ import { CatalogHeaderView } from './views/page/common/CatalogHeaderView';
 import { GetCatalogLayout } from './views/page/layout/GetCatalogLayout';
 import { MarketplacePostOfferView } from './views/page/layout/marketplace/MarketplacePostOfferView';
 
-export const CatalogView: FC<{}> = props =>
-{
+export const CatalogView: FC<{}> = props => {
     const { isVisible = false, setIsVisible = null, rootNode = null, currentPage = null, navigationHidden = false, setNavigationHidden = null, activeNodes = [], searchResult = null, setSearchResult = null, openPageByName = null, openPageByOfferId = null, activateNode = null, getNodeById } = useCatalog();
 
-    useEffect(() =>
-    {
+    useEffect(() => {
         const linkTracker: ILinkEventTracker = {
-            linkReceived: (url: string) =>
-            {
+            linkReceived: (url: string) => {
                 const parts = url.split('/');
-        
-                if(parts.length < 2) return;
-        
-                switch(parts[1])
-                {
+
+                if (parts.length < 2) return;
+
+                switch (parts[1]) {
                     case 'show':
                         setIsVisible(true);
                         return;
@@ -35,27 +31,22 @@ export const CatalogView: FC<{}> = props =>
                         setIsVisible(prevValue => !prevValue);
                         return;
                     case 'open':
-                        if(parts.length > 2)
-                        {
-                            if(parts.length === 4)
-                            {
-                                switch(parts[2])
-                                {
+                        if (parts.length > 2) {
+                            if (parts.length === 4) {
+                                switch (parts[2]) {
                                     case 'offerId':
                                         openPageByOfferId(parseInt(parts[3]));
                                         return;
                                 }
                             }
-                            else
-                            {
+                            else {
                                 openPageByName(parts[2]);
                             }
                         }
-                        else
-                        {
+                        else {
                             setIsVisible(true);
                         }
-        
+
                         return;
                 }
             },
@@ -65,47 +56,51 @@ export const CatalogView: FC<{}> = props =>
         AddEventLinkTracker(linkTracker);
 
         return () => RemoveLinkEventTracker(linkTracker);
-    }, [ setIsVisible, openPageByOfferId, openPageByName ]);
+    }, [setIsVisible, openPageByOfferId, openPageByName]);
 
     return (
         <>
-            { isVisible &&
-                <NitroCardView uniqueKey="catalog" className="nitro-catalog" style={ GetConfiguration('catalog.headers') ? { width: 710 } : {} }>
-                    <NitroCardHeaderView headerText={ LocalizeText('catalog.title') } onCloseClick={ event => setIsVisible(false) } />
+            {isVisible &&
+                <NitroCardView uniqueKey="catalog" className="nitro-catalog" style={GetConfiguration('catalog.headers') ? { width: 710 } : {}}>
+                    <NitroCardHeaderView headerText={LocalizeText('catalog.title')} onCloseClick={event => setIsVisible(false)} />
                     <NitroCardTabsView subClassName="w-100">
-                        { rootNode && (rootNode.children.length > 0) && rootNode.children.map(child =>
-                        {
-                            if(!child.isVisible) return null;
-
-                            return (
-                                <NitroCardTabsItemView key={ child.pageId } isActive={ child.isActive } onClick={ event =>
-                                {
-                                    if(searchResult) setSearchResult(null);
-
-                                    activateNode(child);
-                                } } >
-                                    <Flex gap={ GetConfiguration('catalog.tab.icons') ? 1 : 0 } alignItems="center">
-                                        { GetConfiguration('catalog.tab.icons') && <CatalogIconView icon={ child.iconId } /> }
-                                        { child.localization }
-                                    </Flex>
-                                </NitroCardTabsItemView>
-                            );
-                        }) }
+                        {rootNode &&
+                            rootNode.children.length > 0 &&
+                            rootNode.children.map((child, index) => {
+                                if (!child.isVisible) return null;
+                                // Generate a unique key using the index of the map function
+                                const uniqueKey = `${child.pageId}-${index}`;
+                                return (
+                                    <NitroCardTabsItemView
+                                        key={uniqueKey}
+                                        isActive={child.isActive}
+                                        onClick={(event) => {
+                                            if (searchResult) setSearchResult(null);
+                                            activateNode(child);
+                                        }}
+                                    >
+                                        <Flex gap={GetConfiguration('catalog.tab.icons') ? 1 : 0} alignItems="center">
+                                            {GetConfiguration('catalog.tab.icons') && <CatalogIconView icon={child.iconId} />}
+                                            {child.localization}
+                                        </Flex>
+                                    </NitroCardTabsItemView>
+                                );
+                            })}
                     </NitroCardTabsView>
-                    <CatalogHeaderView node={ rootNode } />
+                    <CatalogHeaderView node={rootNode} />
                     <NitroCardContentView>
                         <Grid>
-                            { !navigationHidden &&
-                                <Column className="catalog-left" size={ 4 } overflow="hidden">
-                                    { activeNodes && (activeNodes.length > 0) &&
-                                        <CatalogNavigationView node={ activeNodes[0] } /> }
-                                </Column> }
-                            <Column size={ !navigationHidden ? 8 : 12 } overflow="hidden" className="ms-n1">
-                                { GetCatalogLayout(currentPage, () => setNavigationHidden(true)) }
+                            {!navigationHidden &&
+                                <Column className="catalog-left" size={4} overflow="hidden">
+                                    {activeNodes && (activeNodes.length > 0) &&
+                                        <CatalogNavigationView node={activeNodes[0]} />}
+                                </Column>}
+                            <Column size={!navigationHidden ? 8 : 12} overflow="hidden" className="ms-n1">
+                                {GetCatalogLayout(currentPage, () => setNavigationHidden(true))}
                             </Column>
                         </Grid>
                     </NitroCardContentView>
-                </NitroCardView> }
+                </NitroCardView>}
             <CatalogGiftView />
             <MarketplacePostOfferView />
         </>
