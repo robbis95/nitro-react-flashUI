3c96a11f68c863e2b271aa82e132c06362b9bf4d
duckietm <troop@mailtjes.com>
2023-08-08 13:04:40 +0200

    Fixed Relations
    Fixed the Relations in Friends screen

--- src/components/friends/views/friends-list/friends-list-group/FriendsListGroupItemView.tsx
+++ src/components/friends/views/friends-list/friends-list-group/FriendsListGroupItemView.tsx
@@ -3,96 +3,153 @@ import { LocalizeText, MessengerFriend, OpenMessengerChat } from '../../../../..
 import { Base, Column, Flex, LayoutAvatarImageView, NitroCardAccordionItemView, Text, UserProfileIconView } from '../../../../../common';
 import { useFriends } from '../../../../../hooks';
 
-interface FriendsListGroupItemViewProps
-{
-    friend: MessengerFriend;
-    selected: boolean;
-    selectFriend: (userId: number) => void;
-    setShowHoverText?: (text: string) => void;
+interface FriendsListGroupItemViewProps {
+  friend: MessengerFriend;
+  selected: boolean;
+  selectFriend: (userId: number) => void;
+  setShowHoverText?: (text: string) => void;
 }
 
-export const FriendsListGroupItemView: FC<FriendsListGroupItemViewProps> = props =>
-{
-    const { friend = null, selected = false, selectFriend = null, setShowHoverText = null } = props;
-    const [ isRelationshipOpen, setIsRelationshipOpen ] = useState<boolean>(false);
-    const { followFriend = null, updateRelationship = null } = useFriends();
+export const FriendsListGroupItemView: FC<FriendsListGroupItemViewProps> = (props) => {
+  const {
+    friend = null,
+    selected = false,
+    selectFriend = null,
+    setShowHoverText = null,
+  } = props;
 
-    const clickFollowFriend = (event: MouseEvent<HTMLDivElement>) =>
-    {
-        event.stopPropagation();
+  const { followFriend = null, updateRelationship = null } = useFriends();
 
-        followFriend(friend);
+  const [openRelationshipForFriends, setOpenRelationshipForFriends] = useState<number[]>([]);
+
+  const isRelationshipOpen = (friendId: number) => openRelationshipForFriends.includes(friendId);
+
+  const toggleRelationship = (friendId: number) => {
+    if (isRelationshipOpen(friendId)) {
+      setOpenRelationshipForFriends((prevOpenRelationships) => prevOpenRelationships.filter((id) => id !== friendId));
+    } else {
+      setOpenRelationshipForFriends((prevOpenRelationships) => [...prevOpenRelationships, friendId]);
     }
+  };
 
-    const openMessengerChat = (event: MouseEvent<HTMLDivElement>) =>
-    {
-        event.stopPropagation();
-        
-        OpenMessengerChat(friend.id);
+  const clickFollowFriend = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+    followFriend(friend);
+  };
+
+  const openMessengerChat = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+    OpenMessengerChat(friend.id);
+  };
+
+  const openRelationship = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+    toggleRelationship(friend.id);
+  };
+
+  const clickUpdateRelationship = (event: MouseEvent<HTMLDivElement>, type: number) => {
+    event.stopPropagation();
+    updateRelationship(friend, type);
+    toggleRelationship(friend.id);
+  };
+
+  const getCurrentRelationshipName = () => {
+    if (!friend) return null;
+
+    switch (friend.relationshipStatus) {
+      case MessengerFriend.RELATIONSHIP_HEART:
+        return 'heart';
+      case MessengerFriend.RELATIONSHIP_SMILE:
+        return 'smile';
+      case MessengerFriend.RELATIONSHIP_BOBBA:
+        return 'bobba';
+      default:
+        return null;
     }
+  };
 
-    const openRelationship = (event: MouseEvent<HTMLDivElement>) =>
-    {
-        event.stopPropagation();
+  if (!friend) return null;
 
-        setIsRelationshipOpen(true);
-    }
-
-    const clickUpdateRelationship = (event: MouseEvent<HTMLDivElement>, type: number) =>
-    {
-        event.stopPropagation();
-
-        updateRelationship(friend, type);
-        
-        setIsRelationshipOpen(false);
-    }
-
-    const getCurrentRelationshipName = () =>
-    {
-        if(!friend) return null;
-
-        switch(friend.relationshipStatus)
-        {
-            case MessengerFriend.RELATIONSHIP_HEART: return 'heart';
-            case MessengerFriend.RELATIONSHIP_SMILE: return 'smile';
-            case MessengerFriend.RELATIONSHIP_BOBBA: return 'bobba';
-            default: return null;
-        }
-    }
-
-    if(!friend) return null;
-
-    return (
-        <NitroCardAccordionItemView justifyContent="between" className={ `friend-tab px-2 py-1 ${ selected && 'selected-user text-white' }` } onClick={ event => selectFriend(friend.id) }>
-            <Flex alignItems="center" gap={ 0 }>
-                { (friend.id > 0 && friend.online) &&
-                    <Flex center pointer className="avatar">
-                        <LayoutAvatarImageView headOnly figure={ friend.figure } direction={ 2 } scale={ 0.5 } />
-                    </Flex>
-                }
-                <Base style={ { marginLeft: (friend.id < 0 || !friend.online) ? '14px' : '' } } onClick={ event => event.stopPropagation() } onMouseEnter={ () => setShowHoverText(LocalizeText('infostand.profile.link.tooltip')) } onMouseLeave={ () => setShowHoverText('') }>
-                    <UserProfileIconView userId={ friend.id } />
-                </Base>
-                <Text variant="black" className="ms-1">{ friend.name }</Text>
-            </Flex>
-            <Flex alignItems="center" gap={ 1 }>
-                <Flex className={ !friend.followingAllowed ? 'pe-4' : 'pe-1' } onClick={ openRelationship } onMouseEnter={ () => setShowHoverText(LocalizeText('infostand.link.relationship')) } onMouseLeave={ () => setShowHoverText('') }>
-                    { (friend.id > 0) && <Base className={ `nitro-friends-spritesheet icon-${ getCurrentRelationshipName() }` } /> }
-                    { (friend.id > 0) && <Base className="icon icon-friendlist_arrow_black_down" /> }
-                </Flex>
-                { friend.followingAllowed && <Base pointer onClick={ clickFollowFriend } className="nitro-friends-spritesheet icon-follow" onMouseEnter={ () => setShowHoverText(LocalizeText('friendlist.tip.follow')) } onMouseLeave={ () => setShowHoverText('') } /> }
-                <Base pointer className="nitro-friends-spritesheet icon icon-friend_message" onClick={ openMessengerChat } onMouseEnter={ () => setShowHoverText(LocalizeText('friendlist.tip.im')) } onMouseLeave={ () => setShowHoverText('') } />
-                { isRelationshipOpen &&
-                    <>
-                        <Column position="absolute" className="select-relation">
-                            <Base pointer className="nitro-friends-spritesheet icon-none" onClick={ event => clickUpdateRelationship(event, MessengerFriend.RELATIONSHIP_NONE) } />
-                            <Base pointer className="nitro-friends-spritesheet icon-heart bg-white w-100" onClick={ event => clickUpdateRelationship(event, MessengerFriend.RELATIONSHIP_HEART) } />
-                            <Base pointer className="nitro-friends-spritesheet icon-smile" onClick={ event => clickUpdateRelationship(event, MessengerFriend.RELATIONSHIP_SMILE) } />
-                            <Base pointer className="nitro-friends-spritesheet icon-bobba bg-white w-100" onClick={ event => clickUpdateRelationship(event, MessengerFriend.RELATIONSHIP_BOBBA) } />
-                        </Column>
-                    </>
-                }
-            </Flex>
-        </NitroCardAccordionItemView>
-    );
-}
+  return (
+    <NitroCardAccordionItemView
+      justifyContent="between"
+      className={`friend-tab px-2 py-1 ${selected ? 'selected-user text-white' : ''}`}
+    >
+      <Flex alignItems="center" gap={0}>
+        {friend.id > 0 && friend.online && (
+          <Flex center pointer className="avatar">
+            <LayoutAvatarImageView headOnly figure={friend.figure} direction={2} scale={0.5} />
+          </Flex>
+        )}
+        <Base
+          style={{ marginLeft: friend.id < 0 || !friend.online ? '14px' : '' }}
+          onClick={(event) => event.stopPropagation()}
+          onMouseEnter={() => setShowHoverText(LocalizeText('infostand.profile.link.tooltip'))}
+          onMouseLeave={() => setShowHoverText('')}
+        >
+          <UserProfileIconView userId={friend.id} />
+        </Base>
+        <Text variant="black" className="ms-1">
+          {friend.name}
+        </Text>
+      </Flex>
+      <Flex alignItems="center" gap={1}>
+        <Flex
+          className={!friend.followingAllowed ? 'pe-4' : 'pe-1'}
+          onClick={openRelationship}
+          onMouseEnter={() => setShowHoverText(LocalizeText('infostand.link.relationship'))}
+          onMouseLeave={() => setShowHoverText('')}
+        >
+          {friend.id > 0 && (
+            <>
+              <Base
+                className={`nitro-friends-spritesheet ${isRelationshipOpen(friend.id) ? 'icon icon-friendlist_arrow_black_down' : 'icon icon-friendlist_arrow_black_right'}`}
+              />
+              {isRelationshipOpen(friend.id) ? null : <Base className={`nitro-friends-spritesheet ${getCurrentRelationshipName()}`} />}
+            </>
+          )}
+        </Flex>
+        {friend.followingAllowed && (
+          <Base
+            pointer
+            onClick={clickFollowFriend}
+            className="nitro-friends-spritesheet icon-follow"
+            onMouseEnter={() => setShowHoverText(LocalizeText('friendlist.tip.follow'))}
+            onMouseLeave={() => setShowHoverText('')}
+          />
+        )}
+        <Base
+          pointer
+          className="nitro-friends-spritesheet icon icon-friend_message"
+          onClick={openMessengerChat}
+          onMouseEnter={() => setShowHoverText(LocalizeText('friendlist.tip.im'))}
+          onMouseLeave={() => setShowHoverText('')}
+        />
+        {isRelationshipOpen(friend.id) && (
+          <Column position="absolute" className="select-relation">
+            <Base
+              pointer
+              className="nitro-friends-spritesheet icon-none"
+              onClick={(event) => clickUpdateRelationship(event, MessengerFriend.RELATIONSHIP_NONE)}
+            />
+            <Base
+              pointer
+              className="nitro-friends-spritesheet icon-heart bg-white w-100"
+              onClick={(event) => clickUpdateRelationship(event, MessengerFriend.RELATIONSHIP_HEART)}
+            />
+            <Base
+              pointer
+              className="nitro-friends-spritesheet icon-smile"
+              onClick={(event) => clickUpdateRelationship(event, MessengerFriend.RELATIONSHIP_SMILE)}
+            />
+            <Base
+              pointer
+              className="nitro-friends-spritesheet icon-bobba bg-white w-100"
+              onClick={(event) => clickUpdateRelationship(event, MessengerFriend.RELATIONSHIP_BOBBA)}
+            />
+          </Column>
+        )}
+      </Flex>
+    </NitroCardAccordionItemView>
+  );
+};
