df4397511b074d7c6d1682ba8d589753725e9768
object <110488133+oobjectt@users.noreply.github.com>
2023-09-27 22:53:21 +0200

    Messenger Friends - FollowFriendFailedEvent added

---
+++ src/api/friends/MessengerFollowFriendFailedType.ts
@@ -0,0 +1,7 @@
+export class MessengerFollowFriendFailedType
+{
+    public static readonly NOT_IN_FRIEND_LIST = 0;
+    public static readonly FRIEND_OFFLINE = 1;
+    public static readonly FRIEND_NOT_IN_ROOM = 2;
+    public static readonly FRIEND_BLOCKED_STALKING = 3;
+}
